682875350b25a6a7c5de132f1448655647989ee6
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-04-14 13:59:22 -0400

    First Commit
    Commit to master of Filter Application for iOS-1.

---
+++ WebCam/Assets/MyScripts/EnableDisable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnableDisable : MonoBehaviour
+{
+
+    public GameObject Enable_Disable;
+
+    public void Enable()
+    {
+        Enable_Disable.SetActive(true);
+    }
+    public void Disable()
+    {
+        Enable_Disable.SetActive(false);
+    }
+
+}
---
+++ WebCam/Assets/MyScripts/WebCam.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    int currentCamIndex = 0;
+
+    WebCamTexture tex;
+
+    public RawImage display;
+
+    public Text startStopText;
+
+    public void SwapCam_Clicked()
+    {
+        if (WebCamTexture.devices.Length > 0)
+        {
+            currentCamIndex += 1;
+            currentCamIndex %= WebCamTexture.devices.Length;
+
+            //if tex is not null:
+            //stop the webcam
+            //start the webcam
+
+            if (tex != null)
+            {
+                StopWebCam();
+                StartStopCam_Clicked();
+            }
+        }
+    }
+
+        public void StartStopCam_Clicked()
+        {
+            if (tex != null) // stop the camera
+            {
+                StopWebCam();
+                startStopText.text = "Start";
+            }
+            else //start the camera
+            {
+                WebCamDevice device = WebCamTexture.devices[currentCamIndex];
+                tex = new WebCamTexture(device.name);
+                display.texture = tex;
+
+                tex.Play();
+                startStopText.text = "Stop";
+            }
+        }
+
+        private void StopWebCam()
+        {
+            display.texture = null;
+            tex.Stop();
+            tex = null;
+        }
+}
